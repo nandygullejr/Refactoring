3fba5da53ff2d4cc9d8bb39f469bfa1a47a22a74
Ferdinand Gulle Jr <nandygullejr@yahoo.com>
2022-11-21 18:37:34 +0800

    Refactor using 'Extract Function' (ref.: https://refactoring.com/catalog/extractFunction.html).

--- javascript.js
+++ javascript.js
@@ -6,12 +6,8 @@ function statement (invoice, plays) {
                           { style: "currency", currency: "USD",
                             minimumFractionDigits: 2 }).format;
     for (let perf of invoice.performances) {
-  
-      // add volume credits
-      volumeCredits += Math.max(perf.audience - 30, 0);
-      // add extra credit for every ten comedy attendees
-      if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
-  
+      volumeCredits += volumeCreditsFor(perf);
+ 
       // print line for this order
       result += `  ${playFor(perf).name}: ${format(amountFor(perf)/100)} (${perf.audience} seats)\n`;
       totalAmount += amountFor(perf);
@@ -44,6 +40,15 @@ function statement (invoice, plays) {
 
     function playFor(aPerformance) {
         return plays[aPerformance.playID];
+    }
+
+    function volumeCreditsFor(perf) {
+      let volumeCredits = 0;
+      // add volume credits
+      volumeCredits += Math.max(perf.audience - 30, 0);
+      // add extra credit for every ten comedy attendees
+      if ("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
+      return volumeCredits;
     }
   }
 
