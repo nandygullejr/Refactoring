77dd7aabb9cb3697ea23ecd396f1fd2a23922ecb
Ferdinand Gulle Jr <nandygullejr@yahoo.com>
2022-11-22 18:34:50 +0800

    Refactor using 'Replace Temp with Query' (ref.: https://refactoring.com/catalog/replaceTempWithQuery.html).

--- javascript.js
+++ javascript.js
@@ -7,11 +7,7 @@ function statement (invoice, plays) {
       result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
       totalAmount += amountFor(perf);
     }
-    let volumeCredits = 0;
-    for (let perf of invoice.performances) {
-      volumeCredits += volumeCreditsFor(perf);
-    }
-
+    let volumeCredits = totalVolumeCredits();
     result += `Amount owed is ${usd(totalAmount)}\n`;
     result += `You earned ${volumeCredits} credits\n`;
     return result;
