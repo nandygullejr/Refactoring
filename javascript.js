d2be74c9af4a4853b4b8aa861939cdfac811401e
Ferdinand Gulle Jr <nandygullejr@yahoo.com>
2022-11-26 07:38:32 +0800

    Replace all references to 'amountFor' in 'renderPlainText'.

--- javascript.js
+++ javascript.js
@@ -40,7 +40,7 @@ function statement(invoice, plays) {
 function renderPlainText(data, plays) {
   let result = `Statement for ${data.customer}\n`;
   for (let perf of data.performances) {
-    result += `  ${perf.play.name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+    result += `  ${perf.play.name}: ${usd(perf.amount)} (${perf.audience} seats)\n`;
   }
   result += `Amount owed is ${usd(totalAmount())}\n`;
   result += `You earned ${totalVolumeCredits()} credits\n`;
@@ -49,7 +49,7 @@ function renderPlainText(data, plays) {
   function totalAmount() {
     let result = 0;
     for (let perf of data.performances) {
-      result += amountFor(perf);
+      result += perf.amount;
     }
     return result;
   }
