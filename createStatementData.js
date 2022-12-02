b623f035c5edcf30acdf0011033b10fcd073ad52
Ferdinand Gulle Jr <nandygullejr@yahoo.com>
2022-12-02 19:19:57 +0800

    Refactor using 'Replace Type Code with Subclasses' (ref.: https://refactoring.com/catalog/replaceTypeCodeWithSubclasses.html).

--- createStatementData.js
+++ createStatementData.js
@@ -50,7 +50,17 @@ export default function createStatementData(invoice, plays) {
   }
 
   function createPerformanceCalculator(aPerformance, aPlay) {
-    return new PerformanceCalculator(aPerformance, aPlay);
+    switch(aPlay.type) {
+      case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+      case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+      default:
+        throw new Error(`unknown type: ${aPlay.type}`);
+    }
+  }
+
+  class TragedyCalculator extends PerformanceCalculator {
+  }
+  class ComedyCalculator extends PerformanceCalculator {
   }
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
