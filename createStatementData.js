7b45b0fe6ba4f65aa45562aee39be7e2d0fb340b
Ferdinand Gulle Jr <nandygullejr@yahoo.com>
2022-12-02 19:43:00 +0800

    Refactor for unaccounted parts.

--- createStatementData.js
+++ createStatementData.js
@@ -15,50 +15,8 @@ export default function createStatementData(invoice, plays) {
     return result;
   }
 
-  class PerformanceCalculator {
-    constructor(aPerformance, aPlay) {
-      this.performance = aPerformance;
-      this.play = aPlay;
-    }
-    get amount() {
-      throw new Error('subclass responsibility');
-    }
-    get volumeCredits() {
-      return Math.max(this.performance.audience - 30, 0);
-    }
-  }
-
-  function createPerformanceCalculator(aPerformance, aPlay) {
-    switch(aPlay.type) {
-      case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
-      case "comedy": return new ComedyCalculator(aPerformance, aPlay);
-      default:
-        throw new Error(`unknown type: ${aPlay.type}`);
-    }
-  }
-
-  class TragedyCalculator extends PerformanceCalculator {
-    get amount() {
-      let result = 40000;
-      if (this.performance.audience > 30) {
-        result += 1000 * (this.performance.audience - 30);
-      }
-      return result;
-    }
-  }
-  class ComedyCalculator extends PerformanceCalculator {
-    get amount() {
-      return super.volumeCredits + Math.floor(this.performance.audience / 5);
-    }
-  }
   function playFor(aPerformance) {
     return plays[aPerformance.playID];
-  }
-  function amountFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
-  }
-  function volumeCreditsFor(aPerformance) {
-    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).volumeCredits; // Git practice for git commit --amend
   }
   function totalAmount(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
@@ -66,5 +24,51 @@ export default function createStatementData(invoice, plays) {
 
   function totalVolumeCredits(data) {
     return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case "tragedy": return new TragedyCalculator(aPerformance, aPlay);
+    case "comedy": return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`unknown type: ${aPlay.type}`);
+  }
+}
+
+class PerformanceCalculator {
+  constructor(aPerformance, aPlay) {
+    this.performance = aPerformance;
+    this.play = aPlay;
+  }
+  get amount() {
+    throw new Error('subclass responsibility');
+  }
+  get volumeCredits() {
+    return Math.max(this.performance.audience - 30, 0);
+  }
+}
+
+class TragedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator {
+  get amount() {
+    let result = 30000;
+    if (this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
+    return result;
+  }
+  get volumeCredits() {
+    return super.volumeCredits + Math.floor(this.performance.audience / 5);
   }
 }
