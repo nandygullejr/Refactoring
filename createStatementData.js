ca8e4c265f4e0f0263de935495126c6bc7a279ef
Ferdinand Gulle Jr <nandygullejr@yahoo.com>
2022-12-02 18:33:03 +0800

    Refactor using 'Move Function' (ref.: https://refactoring.com/catalog/moveFunction.html).

--- createStatementData.js
+++ createStatementData.js
@@ -1,62 +1,65 @@
-export default function createStatementData(invoice,plays) {
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return statementData;
-  
-    function enrichPerformance(aPerformance) {
-      const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
-      const result = Object.assign({}, aPerformance);
-      result.play = calculator.play;
-      result.amount = amountFor(result);
-      result.volumeCredits = volumeCreditsFor(result);
-      return result;
-    }
+export default function createStatementData(invoice, plays) {
+  const statementData = {};
+  statementData.customer = invoice.customer;
+  statementData.performances = invoice.performances.map(enrichPerformance);
+  statementData.totalAmount = totalAmount(statementData);
+  statementData.totalVolumeCredits = totalVolumeCredits(statementData);
+  return statementData;
 
-    class PerformanceCalculator {
-      constructor(aPerformance, aPlay) {
-        this.performance = aPerformance;
-        this.play = aPlay;
-      }
+  function enrichPerformance(aPerformance) {
+    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const result = Object.assign({}, aPerformance);
+    result.play = calculator.play;
+    result.amount = amountFor(result);
+    result.volumeCredits = volumeCreditsFor(result);
+    return result;
+  }
+
+  class PerformanceCalculator {
+    constructor(aPerformance, aPlay) {
+      this.performance = aPerformance;
+      this.play = aPlay;
     }
-  
-    function playFor(aPerformance) {
-      return plays[aPerformance.playID];
-    }
-    function amountFor(aPerformance) {
+    get amount() {
       let result = 0;
-      switch (aPerformance.play.type) {
-      case "tragedy":
-        result = 40000;
-        if (aPerformance.audience > 30) {
-          result += 1000 * (aPerformance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if (aPerformance.audience > 20) {
-          result += 10000 + 500 * (aPerformance.audience - 20);
-        }
-        result += 300 * aPerformance.audience;
-        break;
-      default:
-          throw new Error(`unknown type: ${aPerformance.play.type}`);
+      switch (this.play.type) {
+        case "tragedy":
+          result = 40000;
+          if (this.performance.audience > 30) {
+            result += 1000 * (this.performance.audience - 30);
+          }
+          break;
+        case "comedy":
+          result = 30000;
+          if (this.performance.audience > 20) {
+            result += 10000 + 500 * (this.performance.audience - 20);
+          }
+          result += 300 * this.performance.audience;
+          break;
+        default:
+          throw new Error(`unknown type: ${this.play.type}`);
       }
       return result;
-    }
-    function volumeCreditsFor(aPerformance) {
-      let result = 0;
-      result += Math.max(aPerformance.audience - 30, 0);
-      if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
-      return result;
-    }
-    function totalAmount(data) {
-      return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
-    }
-  
-    function totalVolumeCredits(data) {
-      return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
     }
   }
+
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+  function amountFor(aPerformance) {
+    return new PerformanceCalculator(aPerformance, playFor(aPerformance)).amount;
+  }
+  function volumeCreditsFor(aPerformance) {
+    let result = 0;
+    result += Math.max(aPerformance.audience - 30, 0);
+    if ("comedy" === aPerformance.play.type) result += Math.floor(aPerformance.audience / 5);
+    return result;
+  }
+  function totalAmount(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+
+  function totalVolumeCredits(data) {
+    return data.performances.reduce((total, p) => total + p.volumeCredits, 0);
+  }
+}
